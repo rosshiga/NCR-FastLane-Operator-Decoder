a8fc1bbab63d476a2d5de49c59fbe35f637f62fb
Ross Higa <commits_n_gits@rosshiga.com>
2023-10-09 15:52:32 -1000

    Update web.js

--- web.js
+++ web.js
@@ -1,42 +1,39 @@
 function encoder(password) {
-  var encoded, encoded_chr, encoded_first, first, second;
-  encoded = "";
+    let encoded = "";
+    if (password.length < 4) {
+        encoded = "$";
+        password = "0" + password;
+    } else {
+        const first = password[0];
+        const encodedFirst = String.fromCharCode(first.charCodeAt(0) + 4);
+        encoded += encodedFirst;
+    }
 
-  if (password.length < 4) {
-    encoded = "$";
-    password = "0" + password;
-  } else {
-    first = password[0];
-    encoded_first = chr(ord(first) + 4);
-    encoded = encoded + encoded_first;
-  }
+    for (let i = 1; i < 4; i++) {
+        const second = password[i];
+        const encodedChr = String.fromCharCode(second.charCodeAt(0) + i + 4);
+        encoded += encodedChr;
+    }
 
-  for (var i = 1, _pj_a = 4; i < _pj_a; i += 1) {
-    second = password[i];
-    encoded_chr = chr(ord(second) + i + 4);
-    encoded = encoded + encoded_chr;
-  }
-
-  return encoded;
+    return encoded;
 }
 
+
 function decoder(input) {
-  var char1, decoded, decoded_char, first;
-  decoded = "";
+    let decoded = "";
+    if (input[0] === "$") {
+        decoded = " ";
+    } else {
+        const first = input[0];
+        const decodedChar = String.fromCharCode(first.charCodeAt(0) - 4);
+        decoded += decodedChar;
+    }
 
-  if (input[0] === "$") {
-    decoded = " ";
-  } else {
-    first = input[0];
-    decoded_char = chr(ord(first) - 4);
-    decoded = decoded + decoded_char;
-  }
+    for (let i = 1; i < input.length; i++) {
+        const char2 = input[i];
+        const decodedChar = String.fromCharCode(char2.charCodeAt(0) - 4 - i);
+        decoded += decodedChar;
+    }
 
-  for (var i = 1, _pj_a = 4; i < _pj_a; i += 1) {
-    char1 = input[i];
-    decoded_char = chr(ord(char1) - 4 - i);
-    decoded = decoded + decoded_char;
-  }
-
-  return decoded;
+    return decoded;
 }
